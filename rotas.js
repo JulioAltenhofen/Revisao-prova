736462e5f923db5a885ec08826d6fd517a9e77be
JulioAltenhofen <julioaltenhofen@gmail.com>
2023-04-10 21:40:18 -0300

    Atualização

--- rotas.js
+++ rotas.js
@@ -6,7 +6,15 @@ let lista = [ {"id":1,"nome":"Joao","idade":20},
               {"id":2,"nome":"Maria","idade":19},  
               {"id":3,"nome":"Paulo","idade":21}]
 
-routes.get(req)
+routes.get("/:id",(req,res)=>(
+    res.status(200).json(lista[req.params.id-1])
+))
+            
+
+routes.get("/:id",(req,res)=>(
+    res.status(200).json(lista[req.params.id-1])
+))
+
 
 
 module.exports = routes
